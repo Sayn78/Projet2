b747b70981fe4d930c1932d8a4c1104555d7f0c8
Sayn78 <anthony.senan@laposte.net>
2025-07-20 23:25:10 +0200

    scrip inventory ansible

---
+++ scripts/gen_inventory.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Récupère l'IP publique depuis Terraform
+IP=$(terraform -chdir=terraform output -raw instance_ip)
+
+# Fichier destination
+INV_FILE="ansible/inventory.ini"
+
+# Génère le fichier INI
+cat <<EOF > $INV_FILE
+[k8s]
+$IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/projet2-key.pem
+EOF
+
+echo "✅ Fichier $INV_FILE généré avec l'adresse IP $IP"
