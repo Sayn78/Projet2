e4d6685468deaa93c652e03a663161611a6755b9
Sayn78 <anthony.senan@laposte.net>
2025-07-28 01:08:56 +0200

    kubernetes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,56 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  tags = {
-    Name = "Projet2-VPC"
-  }
-}
-
-# Subnet
-resource "aws_subnet" "main_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "${var.aws_region}a"
-
-  tags = {
-    Name = "Projet2-Subnet"
-  }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  tags = {
-    Name = "Projet2-IGW"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "main_rt" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "Projet2-RouteTable"
-  }
-}
-
-# Route Table Association
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main_subnet.id
-  route_table_id = aws_route_table.main_rt.id
-}
-
 # Security Group
 resource "aws_security_group" "ssh_web" {
   name        = "Projet2-SG"
