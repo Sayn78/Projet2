2345f1c70c3a1cc4eb47cc1e7fa582e188532eb7
Sayn78 <anthony.senan@laposte.net>
2025-07-29 11:34:42 +0200

    EKS terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,62 @@ provider "aws" {
   region = var.aws_region
 }
 
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Projet2-VPC"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+
+  tags = {
+    Name = "Projet2-Subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Projet2-IGW"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "main_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Projet2-RouteTable"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_rt.id
+}
+
 # Security Group
 resource "aws_security_group" "ssh_web" {
   name        = "Projet2-SG"
   description = "Allow Kubernetes, SSH, NodePort and HTTP"
+  vpc_id      = aws_vpc.main_vpc.id
 
-  # SSH
   ingress {
     from_port   = 22
     to_port     = 22
@@ -15,7 +65,6 @@ resource "aws_security_group" "ssh_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP
   ingress {
     from_port   = 80
     to_port     = 80
@@ -23,7 +72,6 @@ resource "aws_security_group" "ssh_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Kubernetes API Server
   ingress {
     from_port   = 6443
     to_port     = 6443
@@ -31,7 +79,6 @@ resource "aws_security_group" "ssh_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # NodePort (application Kubernetes exposée)
   ingress {
     from_port   = 30000
     to_port     = 32767
@@ -39,7 +86,6 @@ resource "aws_security_group" "ssh_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Kubernetes internal components (scheduler, controller, etc.)
   ingress {
     from_port   = 10250
     to_port     = 10259
@@ -47,7 +93,6 @@ resource "aws_security_group" "ssh_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # DNS (si CoreDNS en UDP)
   ingress {
     from_port   = 53
     to_port     = 53
@@ -55,7 +100,6 @@ resource "aws_security_group" "ssh_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Sortie autorisée
   egress {
     from_port   = 0
     to_port     = 0
@@ -68,36 +112,31 @@ resource "aws_security_group" "ssh_web" {
   }
 }
 
+# EKS Cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "projet2-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
-# Instance EC2
-resource "aws_instance" "k8s_node" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  vpc_security_group_ids      = [aws_security_group.ssh_web.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "Projet2-EC2"
+  vpc_config {
+    subnet_ids = [aws_subnet.main_subnet.id]
+    endpoint_public_access = true
   }
 
-
-  provisioner "remote-exec" {
-    inline = ["echo Instance up!"]
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.private_key_path)
-      host        = self.public_ip
-    }
+  kubernetes_network_config {
+    service_ipv4_cidr = "10.100.0.0/16"
   }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy
+  ]
 }
 
-# Créer une Elastic IP
-resource "aws_eip" "k8s_eip" {
-  instance = aws_instance.k8s_node.id
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
 
-  tags = {
-    Name = "Projet2-EIP"
-  }
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
 }
