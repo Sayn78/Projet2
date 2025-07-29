e7fb62363c52a5561fa1c620e2a76b7c9e463d4e
Sayn78 <anthony.senan@laposte.net>
2025-07-29 12:24:22 +0200

    EKS terraform

--- terraform/eks_iam.tf
+++ terraform/eks_iam.tf
@@ -63,3 +63,22 @@ resource "aws_iam_role_policy_attachment" "ec2_container_registry_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+# Node Group
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.main_subnet.id, aws_subnet.secondary_subnet.id]
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  tags = {
+    Name = "eks-node-group"
+  }
+}
