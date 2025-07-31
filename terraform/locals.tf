6106b629fe22834c12be5c93dee1fe11d8844210
Sayn78 <anthony.senan@laposte.net>
2025-07-31 09:22:42 +0200

    EKS terraform

---
+++ terraform/locals.tf
@@ -0,0 +1,8 @@
+locals {
+    env         = "staging"
+    region      = "eu-west-3"
+    zone1       = "eu-west-3a"
+    zone2       = "eu-west-3b"
+    eks_name    = "projet2"
+    eks_version = "1.33"
+}
